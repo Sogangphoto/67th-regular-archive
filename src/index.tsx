635d3c9ee818176f5ab902bf30d1db1780d72d04
안현석 <ajy720@gmail.com>
2024-01-31 14:02:10 +0900

    add: set react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+import App from "./pages/App";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}></Route>),
 );
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
