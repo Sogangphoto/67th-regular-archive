7523da1047264f23fea0dd76ce928eecae99e43f
안현석 <ajy720@gmail.com>
2024-01-31 15:58:25 +0900

    style: navigation opacity by scroll

--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -1,30 +1,56 @@
+import { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Navigation = () => {
-  const Navigation = styled.div`
-    display: flex;
-    width: 100%;
-    justify-content: flex-end;
-    border-bottom: 1px solid black;
-  `;
+const StyledLink = styled(Link)`
+  width: 200px;
+  height: 50px;
+  text-align: center;
+  line-height: 50px;
+`;
 
-  const StyledLink = styled(Link)`
-    width: 200px;
-    height: 50px;
-    text-align: center;
-    line-height: 50px;
-  `;
+const NavContainer = styled.div<{
+  $opacityHeight?: number;
+  $scrollY: number;
+}>`
+  top: 0;
+  left: 0;
+  position: fixed;
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  border-bottom: 1px solid black;
+
+  opacity: ${(p) => (p.$opacityHeight ? p.$scrollY / p.$opacityHeight : 1)};
+`;
+
+const Navigation = ({ opacityHeight }: { opacityHeight?: number }) => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    if (!opacityHeight) return;
+
+    document.addEventListener("scroll", () => {
+      if (window.scrollY < opacityHeight) {
+        setScrollY(window.scrollY);
+      }
+    });
+    return () => {};
+  }, []);
 
   return (
     <>
-      <Navigation id="navigation">
+      <NavContainer
+        id="navigation"
+        $opacityHeight={opacityHeight}
+        $scrollY={scrollY}
+      >
         <StyledLink to="/">Home</StyledLink>
         <StyledLink to="about">About</StyledLink>
         <StyledLink to="archive">Archive</StyledLink>
         <StyledLink to="photographers">Photographers</StyledLink>
         <StyledLink to="thanks">Thanks</StyledLink>
-      </Navigation>
+      </NavContainer>
     </>
   );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,10 +4,15 @@ import Navigation from "../../component/Navigation";
 import styles from "./index.module.css";
 
 function Index() {
+  const TestDiv = styled.div`
+    background-color: skyblue;
+    height: ${window.outerHeight}px;
+  `;
   return (
     <>
-      <Navigation />
+      <Navigation opacityHeight={window.outerHeight} />
       <div className={styles.container}>Home page</div>
+      <div style={{ height: "2000px" }}></div>
     </>
   );
 }
