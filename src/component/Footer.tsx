bb460389f619ce041da4047735e59753c740c9aa
안현석 <ajy720@gmail.com>
2024-02-17 04:04:54 -0500

    add: footer logo

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,8 +1,17 @@
+import sogang from "../assets/sogang_logo.png";
+import sogangPhoto from "../assets/sogangphoto_logo.png";
+
 const Footer = () => {
   return (
     <div className="hidden h-fit flex-wrap items-center space-x-4 p-4 xl:flex">
-      <div className="">서강대 로고</div>
-      <div className="">서광회 로고</div>
+      <div
+        className="h-[50px] w-[100px] bg-contain bg-center bg-no-repeat"
+        style={{ backgroundImage: `url("${sogang}")` }}
+      ></div>
+      <div
+        className="h-[15px] w-[100px] bg-contain bg-center bg-no-repeat"
+        style={{ backgroundImage: `url("${sogangPhoto}")` }}
+      ></div>
       <div className="pretendard flex-grow text-right text-xs font-light leading-normal">
         <p>
           @sogangphoto
